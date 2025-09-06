7d488fed4b8eb4905903bb5b5a888ab5066dd03c
Corey <35475851+cor9@users.noreply.github.com>
2025-09-06 09:44:30 -0700

    Remove 'Terms' link from Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -25,7 +25,6 @@ export default function Navbar() {
         <div className="nav__links">
           <Link to="/examples">Examples</Link>
           <Link to="/pricing">Pricing</Link>
-          <Link to="/terms">Terms</Link>
           {user ? (
             <>
               <Link to="/dashboard">Dashboard</Link>
