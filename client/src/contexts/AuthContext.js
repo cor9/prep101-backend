5a774a630bb1208fb9776cd3092058fb65f9e45b
Corey <35475851+cor9@users.noreply.github.com>
2025-09-09 13:39:05 -0700

    Update AuthContext.js

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { supabase, signIn, signUp, signOut, getCurrentUser } from '../utils/supabase.js';
+import { supabase, signIn, signUp, signOut, getCurrentUser } from '../utils/supabase';
 
 const AuthContext = createContext();
 
